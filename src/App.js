0b0ff8646e74775d43286b43046fcac7c61abf99
Longphihoang <long.p.hoang1@gmail.com>
2020-11-08 21:03:41 -0800

    2 files modified imgs and what not

--- src/App.js
+++ src/App.js
@@ -1,87 +1,224 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
-import {Card, CardHeader, CardContent, CardActionArea, CardActions, Slider, IconButton, Button} from "@material-ui/core";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardContent,
+  CardActionArea,
+  CardActions,
+  Slider,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
 
-
-import RemoveIcon from '@material-ui/icons/Remove';
-import AddIcon from '@material-ui/icons/Add';
+import RemoveIcon from "@material-ui/icons/Remove";
+import AddIcon from "@material-ui/icons/Add";
 
 let embermage = [
   {
-    name: 'Magma Spear',
+    desc: 'You fire a channeled barrage of magma, piecing foes and setting them aflame. Range is 16 meters.',
+    img:
+      "https://raw.githubusercontent.com/Longphihoang/skilltreebuilder/master/img/skillicon_magmaspear.png",
+    name: "Magma Spear",
     rank: 0,
-    mana: [9,10,11,12,13,14,16,18,20,22,25,28,31,34,38],
-    damage: ['35%','34%','35%','36%','37%','38%','39%','40%','41%','42%','43%', '44%', '45%', '50%', '51%'],
-    additional: ['35% chance to burn', '40% chance to burn', '45% chance to burn', '50% chance to burn', '55% chance to burn', '60% chance to burn', '65% chance to burn', '70% chance to burn', '80% chance to burn', '85% chance to burn','90% chance to burn', '95% chance to burn', '100% chance to burn', '100% chance to burn'],
-    tier: ['range extended to 24 meters', 'burn time increased to 6 seconds', 'magma psears ricochet']
-  },
-  {
-    name: 'Magma Spear',
-    rank: 0,
-    mana: [9,10,11,12,13,14,16,18,20,22,25,28,31,34,38],
-    damage: ['35%','34%','35%','36%','37%','38%','39%','40%','41%','42%','43%', '44%', '45%', '50%', '51%'],
-    additional: ['35% chance to burn', '40% chance to burn', '45% chance to burn', '50% chance to burn', '55% chance to burn', '60% chance to burn', '65% chance to burn', '70% chance to burn', '80% chance to burn', '85% chance to burn','90% chance to burn', '95% chance to burn', '100% chance to burn', '100% chance to burn'],
-    tier: ['range extended to 24 meters', 'burn time increased to 6 seconds', 'magma psears ricochet']
+    mana: [9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 25, 28, 31, 34, 38],
+    weaponType: 'weapon dps',
+    damage: [
+      "35%",
+      "34%",
+      "35%",
+      "36%",
+      "37%",
+      "38%",
+      "39%",
+      "40%",
+      "41%",
+      "42%",
+      "43%",
+      "44%",
+      "45%",
+      "50%",
+      "51%"
+    ],
+    additional: [
+      "35% chance to burn",
+      "40% chance to burn",
+      "45% chance to burn",
+      "50% chance to burn",
+      "55% chance to burn",
+      "60% chance to burn",
+      "65% chance to burn",
+      "70% chance to burn",
+      "80% chance to burn",
+      "85% chance to burn",
+      "90% chance to burn",
+      "95% chance to burn",
+      "100% chance to burn",
+      "100% chance to burn"
+    ],
+    tier: [
+      "range extended to 24 meters",
+      "burn time increased to 6 seconds",
+      "magma psears ricochet"
+    ]
   },
     {
-    name: 'Magma Spear',
+    desc: 'You fire a channeled barrage of magma, piecing foes and setting them aflame. Range is 16 meters.',
+    img:
+      "https://raw.githubusercontent.com/Longphihoang/skilltreebuilder/master/img/skillicon_magmaspear.png",
+    name: "Magma Spear",
     rank: 0,
-    mana: [9,10,11,12,13,14,16,18,20,22,25,28,31,34,38],
-    damage: ['35%','34%','35%','36%','37%','38%','39%','40%','41%','42%','43%', '44%', '45%', '50%', '51%'],
-    additional: ['35% chance to burn', '40% chance to burn', '45% chance to burn', '50% chance to burn', '55% chance to burn', '60% chance to burn', '65% chance to burn', '70% chance to burn', '80% chance to burn', '85% chance to burn','90% chance to burn', '95% chance to burn', '100% chance to burn', '100% chance to burn'],
-    tier: ['range extended to 24 meters', 'burn time increased to 6 seconds', 'magma psears ricochet']
+    mana: [9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 25, 28, 31, 34, 38],
+    weaponType: 'weapon dps',
+    damage: [
+      "35%",
+      "34%",
+      "35%",
+      "36%",
+      "37%",
+      "38%",
+      "39%",
+      "40%",
+      "41%",
+      "42%",
+      "43%",
+      "44%",
+      "45%",
+      "50%",
+      "51%"
+    ],
+    additional: [
+      "35% chance to burn",
+      "40% chance to burn",
+      "45% chance to burn",
+      "50% chance to burn",
+      "55% chance to burn",
+      "60% chance to burn",
+      "65% chance to burn",
+      "70% chance to burn",
+      "80% chance to burn",
+      "85% chance to burn",
+      "90% chance to burn",
+      "95% chance to burn",
+      "100% chance to burn",
+      "100% chance to burn"
+    ],
+    tier: [
+      "range extended to 24 meters",
+      "burn time increased to 6 seconds",
+      "magma psears ricochet"
+    ]
+  },
+    {
+    desc: 'You fire a channeled barrage of magma, piecing foes and setting them aflame. Range is 16 meters.',
+    img:
+      "https://raw.githubusercontent.com/Longphihoang/skilltreebuilder/master/img/skillicon_magmaspear.png",
+    name: "Magma Spear",
+    rank: 0,
+    mana: [9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 25, 28, 31, 34, 38],
+    weaponType: 'weapon dps',
+    damage: [
+      "35%",
+      "34%",
+      "35%",
+      "36%",
+      "37%",
+      "38%",
+      "39%",
+      "40%",
+      "41%",
+      "42%",
+      "43%",
+      "44%",
+      "45%",
+      "50%",
+      "51%"
+    ],
+    additional: [
+      "35% chance to burn",
+      "40% chance to burn",
+      "45% chance to burn",
+      "50% chance to burn",
+      "55% chance to burn",
+      "60% chance to burn",
+      "65% chance to burn",
+      "70% chance to burn",
+      "80% chance to burn",
+      "85% chance to burn",
+      "90% chance to burn",
+      "95% chance to burn",
+      "100% chance to burn",
+      "100% chance to burn"
+    ],
+    tier: [
+      "range extended to 24 meters",
+      "burn time increased to 6 seconds",
+      "magma psears ricochet"
+    ]
   }
-]
+];
 export default function App() {
   const [skills, setSkills] = useState(embermage);
 
   let SkillList = skills.map((skill, index) => {
-    let updateskill = (count) => {
+    let updateskill = count => {
       let arr = [...skills];
-      arr[index].rank = Math.min(15, Math.max(0, arr[index].rank=count ));
+      arr[index].rank = Math.min(15, Math.max(0, (arr[index].rank = count)));
       setSkills(arr);
     };
-    return <Skill skill={skill} onchange={updateskill}/>
-  })
+    return <Skill skill={skill} onchange={updateskill} />;
+  });
 
   return (
-    <div>
+    <div class="skill-list">
       {SkillList}
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
 
-
-function Skill ({skill, onchange}) {
-  return <Card style={{width: '250px'}}>
-  <CardHeader
-    avatar={<></>}
-    style={{backgroundColor:'darkred',
-            color:'white'}}
-    title={skill.name}
-  />
-  <CardContent>
-    {skill.rank}
- 
-      <IconButton onClick={()=>onchange(skill.rank + 1)}>
-        <AddIcon/>
-      </IconButton>
-      <IconButton onClick={()=>onchange(skill.rank -1)}>
-        <RemoveIcon/>
-      </IconButton>
-      <Slider
-        value={skill.rank}
-        onChange={(e) => onchange(e.target.value)}
-        defaultValue={skill.rank}
-        aria-labelledby="discrete-slider-small-steps"
-        step={1}
-        marks
-        min={0}
-        max={15}
-        valueLabelDisplay="auto"
+function Skill({ skill, onchange }) {
+  return (
+    <Card>
+      <CardHeader
+        avatar={<img class=".responsive-img" src={skill.img} />}
+        style={{ backgroundColor: "darkred", color: "white" }}
+        title={skill.name}
+        subheader={<div>{skill.rank}/15</div>}
       />
-    </CardContent>
-  </Card>;
+      <CardContent>
+        <Typography color="blue" align="right">
+          {skill.mana[(skill.rank-1 < 0 ? 0: skill.rank -1)]} mp
+        </Typography>
+        <Typography>
+          {skill.desc}
+        </Typography>
+        
+        <Slider
+          value={skill.rank}
+          onChange={e => onchange(e.target.value)}
+          defaultValue={skill.rank}
+          aria-labelledby="discrete-slider-small-steps"
+          step={1}
+          marks
+          min={0}
+          max={15}
+          valueLabelDisplay="auto"
+        />
+        <IconButton
+          style={{ float: "right" }}
+          onClick={() => onchange(skill.rank + 1)}
+        >
+          <AddIcon />
+        </IconButton>
+        <IconButton
+          style={{ float: "right" }}
+          onClick={() => onchange(skill.rank - 1)}
+        >
+          <RemoveIcon />
+        </IconButton>
+      </CardContent>
+    </Card>
+  );
 }
